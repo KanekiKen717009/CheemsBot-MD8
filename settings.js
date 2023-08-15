c8db21fd98da2f44457be730e7316421d2bb3135
KanekiKen717009 <104680210+KanekiKen717009@users.noreply.github.com>
2023-08-15 16:57:56 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['201023246121'] //ur owner number
+global.ownernomer = "201023246121" //ur owner number2
+global.ownername = "Kaneki XIII" //ur owner name
+global.ytname = "Nothing" //ur yt chanel name
+global.socialm = "Nothing" //ur github or insta name
+global.location = "Egypt Om El-Donya" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.botname = "𝑅𝑂𝑆𝐸 𝑋𝐼𝐼𝐼"
+global.ownernumber = '201023246121'
+global.ownername = 'Kaneki XIII'
+global.ownerNumber = ["201023246121"]
+global.ownerweb = "Nothing"
+global.websitex = "Nothing"
+global.wagc = "Nothing"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.wm = "XIII Bot Inc."
+global.botscript = 'F Minor' //script link
+global.packname = "𝑅𝑂𝑆𝐸 𝑋𝐼𝐼𝐼"
+global.author = "Enjoy\n\n"
+global.creator = "Nothing"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
@@ -61,7 +61,7 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 
 //messages
 global.mess = {
-    success: 'Here you go!', 
+    success: '!تم', 
 }
 
 let file = require.resolve(__filename)
